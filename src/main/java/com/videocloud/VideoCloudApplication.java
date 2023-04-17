571cd69f58867aeed4119894785c87cf898a51b5
Laneng <3263617383@qq.com>
2023-04-17 09:00:27 +0800

    添加分页拦截器 添加视频上传阿里云工具类 添加热加载

--- src/main/java/com/videocloud/VideoCloudApplication.java
+++ src/main/java/com/videocloud/VideoCloudApplication.java
@@ -2,15 +2,25 @@ package com.videocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
 
 @SpringBootApplication
-public class VideoCloudApplication {
+@ServletComponentScan
+public class VideoCloudApplication implements ServletContainerInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VideoCloudApplication.class, args);
     }
 
 
-
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+        servletContext.setInitParameter("max-file-size","1000MB");
+        servletContext.setInitParameter("max-request-size","1000MB");
+    }
 }
---
+++ src/main/java/com/videocloud/interceptor/MPConfig.java
@@ -0,0 +1,25 @@
+package com.videocloud.interceptor;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 兰晓东
+ * @create 2023-04-17 8:56
+ * @describe:
+ */
+@Configuration
+public class MPConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+
+
+}
