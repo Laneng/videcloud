69c797d5a13180f7cd3c15725fa4495f4a64a7eb
Saika <875993497@qq.com>
2023-04-17 08:42:10 +0800

    use mybatis-plus

--- src/main/java/com/videocloud/AdminConfig.java
+++ src/main/java/com/videocloud/AdminConfig.java
@@ -15,7 +15,7 @@ public class AdminConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/admin/login").setViewName("admin/login");
-        registry.addViewController("/admin/").setViewName("admin/index");
+        registry.addViewController("/admin").setViewName("admin/index");
         registry.addViewController("/admin/index").setViewName("admin/index");
         registry.addViewController("/admin/user").setViewName("admin/menu/user");
         registry.addViewController("/admin/type").setViewName("admin/menu/type");
